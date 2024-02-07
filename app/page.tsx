0cb6a44e55731f3dd1c330f009eec27f5df5210a
Paul Moulerot <paul.moulerot@isitech.fr>
2024-02-07 15:52:10 +0100

    header

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,8 @@ import Header from '@/app/ui/dashboard/header';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <Header/>
-
-       <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+      <Header/> <br></br><br></br><br></br><br></br>
+  
 
       <div className="w-full">
       <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 w-full md:w-full lg:w-2/3 xl:w-1/2 md:px-20 mx-auto">
