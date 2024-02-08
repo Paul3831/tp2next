91e209aebb1c7d934d74eb47b0ca0ede24f630f0
Paul Moulerot <paul.moulerot@isitech.fr>
2024-02-08 10:15:43 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,27 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import Header from '@/app/ui/dashboard/header';
-import Date from '@/app/ui/dashboard/date';
-import MainComponent from '@/app/ui/dashboard/MainComponent';
+  import { ArrowRightIcon } from '@heroicons/react/24/outline';
+  import Link from 'next/link';
+  import Header from '@/app/ui/dashboard/header';
+  import Date from '@/app/ui/dashboard/date';
+  import MainComponent from '@/app/ui/dashboard/MainComponent';
 
-export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col p-6">
-      <Header/> <br></br><br></br><br></br><br></br>
-  
+  export default function Page() {
+    return (
+      <main className="flex flex-col min-h-screen h-screen bg-gray-800 gap-4">
+       <Header/> 
 
-      <div className="w-full">
-
-
-
-      
-      
-  <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 w-full md:w-full lg:w-2/3 xl:w-1/2 md:px-20 mx-auto">
- 
-  
-  <Date/>
-  
-  
-  <div className="text-blue-700">aaaaaaaaaaaa
-
-  <MainComponent/>
-  <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-    <strong>To invite</strong> someone to your room share the link below{' '}
-  </p>
-
-
-</div> 
-</div>
-
-
-
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+        <div className="flex w-screen flex-grow border-r-4">
+          <div className="order-2 md:order-1 white-square flex-grow flex flex-col justify-between">
+            <Date />
+            <div className="text-blue-700 flex-grow">aaaaaaaaaaaa
+              <MainComponent />
+              <div className="flex-grow flex items-end justify-center">
+                <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+                  <strong>To invite</strong> someone to your room, share the link below{' '}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </main>
-  );
-}
+      </main>
+    );
+  }
--- app/ui/dashboard/header.tsx
+++ app/ui/dashboard/header.tsx
@@ -4,22 +4,15 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4 fixed top-0 left-0 right-0 z-10">   
-    <nav className="flex items-center justify-between mx-auto max-w-full">   
-    <div className="flex items-center space-x-4">         
-    <div className="text-2xl font-bold flex-none"><img src="https://imgur.com/a/a3um1Nq"></img></div></div>        
-    <div className="flex items-center space-x-4 flex-grow justify-around">
-    <Link href="/dashboard"><p className="hover:text-gray-300">Dashboard</p>          
-    </Link>          
-    <Link href="/analytics"><p className="hover:text-gray-300">Analytics</p>          
-    </Link>          
-    <Link href="/extensions"><p className="hover:text-gray-300">Extensions</p>          
-    </Link></div>        
-    <div className="flex items-center space-x-4">          
-    <Link href="/login"><p className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">              
-    <span>Log in</span></p>          
-    </Link></div></nav></header>
-  );
+    <header className="flex items-center p-x-10 justify-between">
+      <Link href="/dashboard">Dashbord</Link>
+      <Link href="/analytics">Analytics</Link>
+      <Link href="/extensions">Extenssion</Link>
+      <Link href="/login"><p className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
+        <span>Log in</span></p>
+      </Link>
+    </header>
+  )
 };
 
 export default Header;
