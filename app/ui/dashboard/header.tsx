f38e02929039521de70b188097a84db975e2d1ce
Paul Moulerot <paul.moulerot@isitech.fr>
2024-02-08 10:58:03 +0100

    header update

--- app/ui/dashboard/header.tsx
+++ app/ui/dashboard/header.tsx
@@ -4,10 +4,13 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
 const Header = () => {
   return (
-    <header className="flex items-center p-x-10 justify-between">
-      <Link href="/dashboard">Dashbord</Link>
-      <Link href="/analytics">Analytics</Link>
-      <Link href="/extensions">Extenssion</Link>
+    <header className="flex items-center p-x-10 text-white justify-between">
+      <div className='logo'>Logo</div>
+      <div className='flex items-center gap-4'>
+        <Link href="/dashboard">Dashbord</Link>
+        <Link href="/analytics">Analytics</Link>
+        <Link href="/extensions">Extenssion</Link>
+      </div>
       <Link href="/login"><p className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
         <span>Log in</span></p>
       </Link>
